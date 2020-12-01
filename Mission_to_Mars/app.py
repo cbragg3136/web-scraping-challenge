2450fd8be602d29aeb68a26531139e887daa64ad
cbragg3136 <cbragg3136@gmail.com>
2020-11-30 20:19:26 -0500

    commit

--- Mission_to_Mars/app.py
+++ Mission_to_Mars/app.py
@@ -1,26 +1,34 @@
+# import dependencies
 from flask import Flask, render_template, redirect
 from flask_pymongo import PyMongo
 import scrape_mars
 
+# Create an instance of Flask
 app = Flask(__name__)
 
+# Use PyMongo to establish Mongo connection
 app.config['MONGO_URI'] = 'mongodb://localhost:27017/mars_db'
 mongo = PyMongo(app)
 
+# Route to render index.html template using data from Mongo
 @app.route('/')
 def index():
     mars_latest = mongo.db.collection.find_one()
 
     return render_template('index.html', data = mars_latest)
 
-
+# Route that will trigger the scrape function
 @app.route('/scrape')
 def scrape():
+
+    # Run the scrape function
     mars_data = scrape_mars.scrape()
+
+    # Update the Mongo database using update and upsert=True
     mongo.db.collection.update({}, mars_data, upsert=True)
 
+    # Redirect back to home page
     return redirect('/')
-
 
 if __name__ == "__main__":
     app.run(debug=True)
--- Mission_to_Mars/scrape_mars.py
+++ Mission_to_Mars/scrape_mars.py
@@ -1,3 +1,4 @@
+# import dependencies
 from splinter import Browser
 from bs4 import BeautifulSoup
 import pandas as pd
@@ -13,12 +14,15 @@ def scrape():
 
 #NASA Mars News
 
+    #Visit mars.nasa.gov/news
     url = 'https://mars.nasa.gov/news/'
     browser.visit(url)
 
+    #Scrape page into Soup
     html = browser.html
     soup = BeautifulSoup(html, 'html.parser')
 
+    #Get title and paragraph of latest news article
     news_title = soup.find('div', class_='list_text').find('div', class_='content_title').text
     news_p = soup.find('div', class_='article_teaser_body').text
 
@@ -26,18 +30,20 @@ def scrape():
     url = 'https://www.jpl.nasa.gov/spaceimages/?search=&category=Mars'
     browser.visit(url)
 
+    # Click links to retrieve html of large image
     browser.links.find_by_partial_text('FULL IMAGE').click()
     browser.links.find_by_partial_text('more info').click()
 
     html = browser.html
     soup = BeautifulSoup(html, 'html.parser')
 
+    # Find the image url for the current Featured Mars Image and assign the url string
     url = soup.find('figure', class_='lede').find('a')['href']
     featured_image_url = 'https://www.jpl.nasa.gov' + url
     featured_image_url
 
 #Mars Facts
-
+    # Use Pandas to scrape the table of mars facts
     url = 'https://space-facts.com/mars/'
     table = pd.read_html(url)[0]
     table.columns=["Description", "Mars"]
@@ -51,6 +57,7 @@ def scrape():
     html = browser.html
     soup = BeautifulSoup(html, 'html.parser')
 
+    # Scrape and List the names of the 4 hemispheres
     title = soup.find_all('h3')
     hemispheres = [x.text.split(' Enhanced')[0] for x in title]
 
@@ -58,12 +65,13 @@ def scrape():
 
     for hemisphere in hemispheres:
         hem_dict = {}
-
+    # Click each of the links to the hemispheres in order to find the image url to the full resolution image
         browser.links.find_by_partial_text(hemisphere).click()
 
         html = browser.html
         soup = BeautifulSoup(html, 'html.parser')
 
+    # Save image url string and the Hemisphere title containing the hemisphere name in a Python dictionary
         hem_dict['title'] = soup.find('h2', class_='title').text.replace("Enhanced","").strip()
         hem_dict['img_url'] = soup.find('div', class_='downloads').find('a')['href']
         hemisphere_image_urls.append(hem_dict)
@@ -75,6 +83,7 @@ def scrape():
 
     browser.quit()
 
+    # Store all the scraped data in a dictionary
     mars_data = {
         'news_title': news_title,
         'news_p': news_p,
